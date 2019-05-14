ad985614f09c0c30a7a3bd921bdbab412919e8e2
festusogundele <festusogundele9@gmail.com>
2019-05-14 04:24:54 +0100

    i work on route

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,12 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
+
+
+$route['default_controller'] = 'pages/view';
+
 $route['posts/create'] = 'posts/create';
 $route['posts/update'] = 'posts/update';
 $route['posts/(:any)']='posts/view/$1';
 $route['posts'] = 'posts/index';
 
-$route['default_controller'] = 'pages/view';
+//$route['default_controller'] = 'pages/view';
 
 $route['categories'] ='categories/index';
 $route['categories/create']='categories/create';
